3c1ec7b6b00e4a25dcec82e75a34be2971b07af5
Harald von Waldow <harald.vonwaldow@eawag.ch>
2016-10-24 17:56:00 +0200

    unicode / cleanup

--- xlsxtocsv/__main__.py
+++ xlsxtocsv/__main__.py
@@ -1,2 +1,2 @@
-from xlsxtocsv import main
+from .xlsxtocsv import main
 main()
--- xlsxtocsv/xlsxtocsv.py
+++ xlsxtocsv/xlsxtocsv.py
@@ -30,7 +30,13 @@ def parseargs():
     args = pa.parse_args(sys.argv[1:])
     return vars(args)
 
-def stringify(data):
+def _stringify(dat):
+    if not isinstance(dat, basestring):
+        return str(dat)
+    else:
+        return dat.encode('utf-8')
+
+def _transmap(dat):
     transmap = {
         # empty cells are going to be empty strings
         None: 'NA',
@@ -39,33 +45,39 @@ def stringify(data):
         dt.datetime(1899, 12, 30, 0, 0): dt.time(0, 0),
         dt.datetime(1899, 12, 31, 0, 0): dt.datetime(1900, 1, 1, 0, 0),
     }
-    newdata = []
-    for l in data:
-        nl = [transmap[f] if f in transmap else f for f in l]
-        nl = [f.encode('utf-8')
-              if isinstance(f, str) else f for f in nl]
-        newdata.append([str(f) if not isinstance(f, str)
-                        else f for f in nl])
-    return newdata
+    return transmap[dat] if dat in transmap else dat
 
+def _datefix(dat):
+    # if typ is datetime.datetime and time-part is 0:0:0,
+    # covert to datetime.date (assume xlsx cell-type is "Date").
+    if (type(dat) == dt.datetime and
+        (dat.hour, dat.minute, dat.second) == (0, 0, 0)):
+        dat = dat.date()
+    return dat
+
+def transform(l):
+    l = [_transmap(f) for f in l]
+    l = [_datefix(f) for f in l]
+    l = [_stringify(f) for f in l]
+    return l
+    
 def write_csv(data, outfile):
     with open(outfile, 'wb') as fout:
         writer = csv.writer(fout, dialect='RFC4180')
         writer.writerows(data)
 
 def main():
-    csv.register_dialect('RFC4180', RFC4180)
+    csv.register_dialect(u'RFC4180', RFC4180)
     xlsxfile = parseargs()['file']
-    out_prefix = os.path.splitext(xlsxfile)[0]
+    out_prefix = os.path.splitext(os.path.basename(xlsxfile))[0]
     wb = op.load_workbook(xlsxfile, data_only=True)
     for sn in wb.sheetnames:
         outfile = out_prefix + '_' + re.sub(r'\s+', '_', sn) + '.csv'
         data = []
         sheet = wb.get_sheet_by_name(sn)
         for l in sheet.values:
-            data.append(l)
-        newdata = stringify(data)
-        write_csv(newdata, outfile)
+            data.append(transform(l))
+        write_csv(data, outfile)
 
 
 if __name__ == '__main__':
